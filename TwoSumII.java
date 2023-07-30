4bc7c49ee02c7e303f4e769ae292c87b5bb61419
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-29 22:56:41 -0700

    Update TwoSumII.java

--- TwoSumII.java
+++ TwoSumII.java
@@ -15,3 +15,28 @@ Example 2:
 Input: numbers = [2,3,4], target = 6
 Output: [1,3]
 Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.length - 1;
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+            if(sum == target) return new int[]{left+1, right+1};
+            else if(sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+
+            
+        }
+
+
+         return new int[0];
+        
+    }
+}
