985531e6b9a33d242cca461e0847f8f2a0ee3816
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-30 23:16:00 -0700

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -13,11 +13,12 @@ nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
 The distinct triplets are [-1,0,1] and [-1,-1,2].
 Notice that the order of the output and the order of the triplets does not matter.
 
+// Time Limit Exceeded Solution
 class Solution {
+  // Tc: O(n^3) and Sc: O(n) 
   public List<List<Integer>> threeSum(int[] nums) {
     List<List<Integer>> ans = new ArrayList<>();
         int n = nums.length;
-        
         for (int i = 0; i < n - 2; i++) {
             for (int j = i + 1; j < n - 1; j++) {
                 for (int k = j + 1; k < n; k++) {
