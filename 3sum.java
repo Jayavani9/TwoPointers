d923e8be539d10f243254592def63ccdb4556851
Jayavani9 <51267675+Jayavani9@users.noreply.github.com>
2023-07-30 23:13:10 -0700

    Update 3sum.java

--- 3sum.java
+++ 3sum.java
@@ -15,4 +15,24 @@ Notice that the order of the output and the order of the triplets does not matte
 
 class Solution {
   public List<List<Integer>> threeSum(int[] nums) {
+    List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        
+        for (int i = 0; i < n - 2; i++) {
+            for (int j = i + 1; j < n - 1; j++) {
+                for (int k = j + 1; k < n; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(triplet);
+                        if (!ans.contains(triplet)) {
+                            ans.add(triplet);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return ans;
+  }
+}
     
